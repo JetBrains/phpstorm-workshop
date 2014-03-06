7794d7e9e0d8c4a1be0dea757e25c9ccf316ac1f
Maarten Balliauw <maarten@balliauw.be>
2014-03-06 12:36:39 +0100

    Added PhpStorm 8 - Multiple Selection

---
+++ 02 - Editing/07 - Rearrange Reformat/7.1. Rearrange Reformat.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Rearrange/Reformat Code
+ *
+ * Rearranges entries in code according to settings. Reformats all code according to code style.
+ *
+ * Ctrl+Alt+L (Windows/Linux)
+ * Alt+Command+L (Mac OS X)
+ */
+
+namespace Editing7\JetBrains;
+
+use Editing2;
+use Editing6;
+
+// 1. The class below is a mess.
+//    - Public/protected/static members are all mixed.
+//    - Brackets are inconsistent
+//    - Indents are inconsistent
+// 2. Use Code | Rearrange Code menu to reorganize the class below.
+// 3. Use undo and roll back the Rearrange Code action.
+// 4. There are also some unused imports (Editing2 and Editing6). The Code | Optimize Imports menu will solve this.
+// 5. Use undo and roll back the Optimize Imports action.
+// 6. Using the keyboard shortcut, Reformat Code and run both rearrange as well as optimize imports in one go.
+//    Note that if desired, the Reformat Code action can be run on the entire project at once.
+// 7. Open the settings and navigate to Project Settings | Code Style | PHP | Arrangement.
+//    Modify the settings so fields are ordered by visibility and then by name. Rearrange Code to test the new settings.
+class Rearrange {
+    protected $_firstProperty;
+    public $_secondProperty;
+    public static $_thirdProperty;
+    protected $_fourthProperty;
+
+    public function firstFunction() {
+        for ($i = 0; $i < 10; $i ++)
+                    {
+            echo 'Test';
+        }
+    }
+
+    public function setFirstProperty($firstProperty) {
+            $this->_firstProperty = $firstProperty;
+    }
+
+    public function getFirstProperty()
+    {
+                    return $this->_firstProperty;
+    }
+
+    public function setSecondProperty($secondProperty)
+    {
+        $this->_secondProperty = $secondProperty;
+    }
+
+    public function getSecondProperty()
+    {
+        return $this->_secondProperty;
+    }
+
+    public static function setThirdProperty($thirdProperty)
+    {
+        self::$_thirdProperty = $thirdProperty;
+    }
+
+    public static function getThirdProperty()
+    {
+        return self::$_thirdProperty;
+    }
+
+    public function setFourthProperty($fourthProperty)
+    {
+        $this->_fourthProperty = $fourthProperty;
+    }
+
+    public function getFourthProperty()
+    {
+        return $this->_fourthProperty;
+    }
+}
---
+++ 02 - Editing/08 - Multiple Selection/1.1. Multiple selection in PHP.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Multiple Selection in PHP
+ *
+ * Put the cursor in multiple locations in a file and write code simultaneously in all these positions.
+ *
+ * Press and hold Alt+Shift and use the mouse to select the locations where a cursor is desired.
+ * Escape to revert back to one cursor. (Windows/Linux/Mac OS X)
+ *
+ * Select Next Occurrence:
+ *   - Alt+J (Windows/Linux)
+ *   - Ctrl-G (Mac OS X)
+ */
+
+// 1. Check below. This is all plain text, we would like add all resources to an array.
+//    Delete the first line and the blank line so only the actual resources remain.
+// 2. Place the cursor right after the first *. Press and hold Alt+Shift and use the mouse to select
+//    the locations where a cursor is desired, right after every *.
+// 3. Release Alt+Shift and start typing. Text should now appear at every cursor. Use backspace to remove
+//    all *. Add ' at the start of each line. Move the cursors to the end of the line and add ',
+// 4. Hit Escape to revert back to one cursor. Select the | in the first line. Use Select Next Occurrence
+//    to select all | characters and have a cursor there. Remove the character and replace it with ', '
+//    Move the cursors to the end of the line and press the delete key. This will move all resources to just one line.
+// 5. Hit Escape to revert back to one cursor and add array( at the beginning of the line and ); at the end. We now have an array.
+// 6. Hold Alt+Shift and drag the mouse from the number 6 in front to the // 1.. Note that the multiple selection now selected
+//    a block of text we can work with.
+
+PhpStorm Resources
+
+* Blog | http://blog.jetbrains.com/phpstorm
+* Twitter | http://twitter.com/phpstorm
+* Video tutorials | http://www.jetbrains.com/phpstorm/documentation/phpstorm-video-tutorials.jsp
+* Tutorials | http://confluence.jetbrains.com/display/PhpStorm/Tutorials
