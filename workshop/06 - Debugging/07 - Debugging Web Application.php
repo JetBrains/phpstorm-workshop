9c993fe52cb524826a198a64a0804c95446a5372
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2016-02-15 15:20:20 +0100

    Adjust web debug workshop for vagrant box

--- workshop/06 - Debugging/07 - Debugging Web Application.php
+++ workshop/06 - Debugging/07 - Debugging Web Application.php
@@ -12,18 +12,10 @@ namespace Debugging7\JetBrains;
 //
 //    If you prefer using a simple browser extension, refer http://confluence.jetbrains.com/display/PhpStorm/Browser+Debugging+Extensions
 //    and install one. For example for Chrome, install https://chrome.google.com/webstore/detail/xdebug-helper/eadndfjplgieldjbigjakmdgkmoaaaoc.
-// 2. Create a new run configuration for the current script.
-//    Select PHP Built-in Web Server, give the configuration a name.
-//    Set the host to 127.0.0.1, port to 789.
-//    Set the document root to the folder in which the 07 - Debugging Web Application.php file lives.
-//    We can now host the current script by running the configuration.
-// 3. Run the configuration and open http://127.0.0.1:789/07 - Debugging Web Application.php.
+// 2. Open http://192.168.56.102/workshop/06 - Debugging/07 - Debugging Web Application.php
 // 4. In PhpStorm, use the Run | Start Listen for PHP Debug Connections menu.
 // 5. In the browser, use the Start Debugger bookmarklet. Click the "Show Greeting" button and note that PhpStorm informs about a new debug connection.
 //    Place some additional breakpoints and see that the Variables view in the Debug Tool Window now holds additional globals ($_COOKIE, $_GET, $_REQUEST).
-
-// Troubleshooting: if PhpStorm does not accept the debuggign connection, make sure that under Project Settings | PHP | Debug,
-// external connections are enabled and tha the number of allowed connections is >= 1.
 ?>
 <!DOCTYPE html>
 <html>
