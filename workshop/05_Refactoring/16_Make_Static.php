7c44f936ad78f48da6020fbf1c1fb82f5fc6be41
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2016-02-17 16:14:46 +0100

    Add lesson for simple Make Static refactoring

---
+++ workshop/05_Refactoring/16_Make_Static.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Make Static
+ *
+ * Make method static.
+ *
+ */
+
+namespace Refactoring16\JetBrains;
+
+// 1. Make method split static.
+class Splitter
+{
+	private $delimiter;
+
+	public function __construct($delimiter)
+	{
+		$this->delimiter = $delimiter;
+	}
+
+	public function split($subject)
+	{
+		return explode($this->delimiter, $subject);
+	}
+}
+
+// 2. Note that calls to Splitter::split() have updated to static calls
+// as well as a new argument with instance have been provided.
+$splitter = new Splitter(' ');
+$myArray = $splitter->split('This is a test');
+$myCsvRecord = $splitter->split('PhpStorm JetBrains');
