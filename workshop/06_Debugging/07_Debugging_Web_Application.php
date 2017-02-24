658cad02c6a44ff1a284c10e062e86710eaa81ec
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2017-02-24 16:27:34 +0100

    Minor fixes for instructions and configurations

--- workshop/06_Debugging/07_Debugging_Web_Application.php
+++ workshop/06_Debugging/07_Debugging_Web_Application.php
@@ -7,15 +7,16 @@
 
 namespace Debugging7\JetBrains;
 
-// 0. Open docker-compose.yml and change remote_host=<ip> to your ip.
-// 1. Visit http://www.jetbrains.com/phpstorm/marklets/ and generate bookmarklets for your debugger (Zend Debugger or Xdebug).
+// 0. Make sure that you've entered the valid IP address in  docker-compose.yml
+// 1. Make sure that you've executed "docker-compose up"
+// 2. Visit http://www.jetbrains.com/phpstorm/marklets/ and generate bookmarklets for your debugger (Zend Debugger or Xdebug).
 //    Drag at least the Start Debugger bookmarklet to the bookmarks bar.
 //
 //    If you prefer using a simple browser extension, refer http://confluence.jetbrains.com/display/PhpStorm/Browser+Debugging+Extensions
 //    and install one. For example for Chrome, install https://chrome.google.com/webstore/detail/xdebug-helper/eadndfjplgieldjbigjakmdgkmoaaaoc.
-// 2. Open http://localhost/07_Debugging_Web_Application.php
-// 3. In PhpStorm, use the Run | Start Listen for PHP Debug Connections menu.
-// 4. In the browser, use the Start Debugger bookmarklet. Click the "Show Greeting" button and note that PhpStorm informs about a new debug connection.
+// 3. Open http://localhost/07_Debugging_Web_Application.php
+// 4. In PhpStorm, use the Run | Start Listen for PHP Debug Connections menu.
+// 5. In the browser, use the Start Debugger bookmarklet. Click the "Show Greeting" button and note that PhpStorm informs about a new debug connection.
 //    Place some additional breakpoints and see that the Variables view in the Debug Tool Window now holds additional globals ($_COOKIE, $_GET, $_REQUEST).
 ?>
 <!DOCTYPE html>
