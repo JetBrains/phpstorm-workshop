982042e758d9204dfda1a88296ebada57b053d17
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2017-02-28 12:56:17 +0100

    Fix test sample for PHPUnit 6

--- workshop/08_Testing/01_Writing_Tests.php
+++ workshop/08_Testing/01_Writing_Tests.php
@@ -10,18 +10,12 @@
 
 namespace Testing\JetBrains;
 
-// 1. Create a new PHPUnit test file. From the navigation bar, create a new file using the PHPUnit template.
-//    Note that another way of creating a test is to use the Go to Test navigation:
-//      - Navigate to the Queue class.
-//      - Use the Go to Test navigation to either go to test or create a new test.
+// 1. Create a new PHP file. From the navigation bar, create a new file using the PHP template.
 //
-// 2. For Fully Qualified name, search for the Queue class.
-//    All other parameters for the unit test class can be kept at their defaults.
-//    Note you can specify the test class name and namespace as well as the directory.
+// 2. Paste the following code
 //
-//    The created test class will look something like the following:
-//
-//    use PHPUnit\Framework\TestCase;
+//    namespace Testing\JetBrains;
+//    use Testing\JetBrains\Queue;
 //    class QueueTest extends TestCase {
 //
 //    }
