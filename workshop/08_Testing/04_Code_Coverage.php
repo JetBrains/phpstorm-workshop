aae16031245b2e74db59fde50e0287a3f2b31a08
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2017-02-28 13:37:31 +0100

    Fix CodeCoverage for PHPUnit 5+

--- workshop/08_Testing/04_Code_Coverage.php
+++ workshop/08_Testing/04_Code_Coverage.php
@@ -7,7 +7,7 @@
 
 namespace Testing\JetBrains;
 
-// 1. Run the unit tests created earlier with Code Coverage enabled.
-//    This can be done using the "Run tests with Coverage" action (try Search Everywhere or the toolbar).
+// 1. Run the unit tests created earlier using QueueTest with Coverage configuration.
+//    There whitelist is configured which is necessary for PHPUnit 5+.
 // 2. Explore code coverage results from the Code Coverage tool window.
 //    Using F4 or double-clicking, navigate to Queue.php and see which lines of code are tested and which ones are not.
