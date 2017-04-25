f78129a2acf17bff2eed10bb186d4188bcbb6965
Gary <gary@hock.in>
2017-04-25 15:22:03 +0200

    Tidied 3.4
    (cherry picked from commit 9e942fd)

--- workshop/01_Navigation/03_Navigating_to_somwhere_we_have_been/3.4_Recent_edits.php
+++ workshop/01_Navigation/03_Navigating_to_somwhere_we_have_been/3.4_Recent_edits.php
@@ -2,21 +2,24 @@
 /**
  * Navigate to Last Edit Location.
  *
- * Navigate back to where code was last edited.
- *
  * Ctrl+Shift+Backspace (Windows/Linux)
  * Shift+Command+Delete (Mac OS X)
+ *
+ * Navigate back to where code was last edited.
  */
 
 namespace Navigation3\JetBrains;
 
-class NavigateRecent {
-    public function startingPoint() {
+class NavigateRecent
+{
+    public function startingPoint()
+    {
         // 1. Navigate to step1().
         $this->step1();
     }
 
-    public function step1() {
+    public function step1()
+    {
         // 2. Uncomment the following line.
         // echo 'In step 1.';
 
@@ -24,7 +27,8 @@ class NavigateRecent {
         $this->step2();
     }
 
-    public function step2() {
+    public function step2()
+    {
         // 4. Navigate back to where we last edited using the keyboard shortcut.
     }
 }
