c4c7d6b186f7ebf3e3ea85a6261a8d94322916fd
Gary <gary@hock.in>
2017-04-26 12:28:43 +0200

    Fixed 5.2 and reformatted code
    (cherry picked from commit 45fea26)

--- workshop/01_Navigation/05_Find_usages/5.2_Find_Usages.php
+++ workshop/01_Navigation/05_Find_usages/5.2_Find_Usages.php
@@ -2,17 +2,20 @@
 /**
  * Find Usages
  *
- * Highlight usages of a given symbol in the current project.
- *
  * Alt+F7 (Windows/Linux/Mac OS X)
  *
- * Ctrl+Alt+F7 shows a popup instead (Windows/Linux)
- * Alt+Command+F7 shows a popup instead (Mac OS X)
+ * Show Usages (uses popup)
+ *
+ * Ctrl+Alt+F7 (Windows/Linux)
+ * Alt+Command+F7 (Mac OS X)
+ *
+ * Highlight usages of a given symbol in the current project.
  */
 
 namespace Navigation5\JetBrains;
 
-class FindUsages {
+class FindUsages
+{
     const MIN_VALUE = -3.5;
     const MAX_VALUE = 3.5;
 
@@ -20,8 +23,9 @@ class FindUsages {
     //    Note the tool window that opens. We can navigate to usages from this tool window.
     //    Filtering is also possible from this tool window.
     //
-    // 2. Place the caret on adjust() and Find Usages using the popup.
-    public function adjust($original, $delta) {
+    // 2. Place the caret on adjust() and Show Usages using the popup.
+    public function adjust($original, $delta)
+    {
         $newValue = $original + $delta;
 
         if ($newValue < self::MIN_VALUE) {
