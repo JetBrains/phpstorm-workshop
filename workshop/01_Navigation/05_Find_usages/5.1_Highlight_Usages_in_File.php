6008beaad42252870cf30ccf9aa037d71807fec8
Gary <gary@hock.in>
2017-04-26 12:23:41 +0200

    Fixed 5.1
    (cherry picked from commit 4036bef)

--- workshop/01_Navigation/05_Find_usages/5.1_Highlight_Usages_in_File.php
+++ workshop/01_Navigation/05_Find_usages/5.1_Highlight_Usages_in_File.php
@@ -2,26 +2,28 @@
 /**
  * Highlight Usages in File
  *
- * Highlight usages of a given symbol in the current file.
- *
  * Ctrl+Shift+F7 (Windows/Linux)
  * Shift+Command+F7 (Mac OS X)
+ *
+ * Highlight usages of a given symbol in the current file.
  */
 
 namespace Navigation5\JetBrains;
 
-class HighlightUsagesInFile {
+class HighlightUsagesInFile
+{
     const MIN_VALUE = -3.5;
     const MAX_VALUE = 3.5;
 
     // 1. Place the caret on $newValue and see the usages are automatically underlined.
     //    Note the different colour highlights for read and write usage.
     //
-    // 2. Place the caret on $newValue and Highlight Usages in File.
+    // 2. Place the caret on $newValue and use Highlight Usages in File.
     //    Note the different colour highlights for read and write usage.
     //    Note the highlights in the stripe on the right of the editor.
     //    Escape clears the highlight.
-    public function adjust($original, $delta) {
+    public function adjust($original, $delta)
+    {
         $newValue = $original + $delta;
 
         if ($newValue < self::MIN_VALUE) {
