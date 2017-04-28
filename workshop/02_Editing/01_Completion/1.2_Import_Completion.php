c3631812aa42cb7540ae262d34779212f621d984
Gary <gary@hock.in>
2017-04-28 08:35:27 +0100

    Rewrote 02.1.2

--- workshop/02_Editing/01_Completion/1.2_Import_Completion.php
+++ workshop/02_Editing/01_Completion/1.2_Import_Completion.php
@@ -1,21 +1,22 @@
 <?php
 /**
- * Import Completion
- *
- * Basic code completion when using classes or functions from other namespaces.
- * Automatically adds import when selected.
+ * Basic Completion With Import
  *
  * Ctrl+Space (Windows/Linux/Mac OS X)
  *
  * Ctrl+Shift+Enter to complete statement (Windows/Linux)
  * Shift+Command+Enter to complete statement (Mac OS X)
+ *
+ * When you use basic completion with classes or functions from outside this
+ * namespace, the `use` statements are added automatically.
  */
 
 namespace Editing1\JetBrains;
 
-// 1. Make the class implement ICustomer. Using import completion will add:
-//       use Navigation4\JetBrains\Customers\ICustomer;
+// 1. Make the class implement ICustomer (place the caret after implements)
+//     Using basic completion will add
+//         `use Navigation4\JetBrains\Customers\ICustomer;`
 
-class ImportCompletion
-    implements // ICustomer {
+class ImportCompletion implements
+{
 }
