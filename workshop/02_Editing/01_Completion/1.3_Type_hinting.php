1ffe098ab6edb919a0ac2e93626f37239fcd5970
Gary <gary@hock.in>
2017-04-28 09:50:51 +0200

    Rewrote 02.1.3
    (cherry picked from commit e344d7c)

--- workshop/02_Editing/01_Completion/1.3_Type_hinting.php
+++ workshop/02_Editing/01_Completion/1.3_Type_hinting.php
@@ -1,8 +1,15 @@
 <?php
 /**
- * Type hinting
+ * Type Hinting
  *
  * Hint PhpStorm about the types you are working with.
+ *
+ * Quick Documentation
+ *
+ * Ctrl+Q (Windows/Linux)
+ * F1 (Mac OS X)
+ *
+ * View quick documentation information about the variable, class or method
  */
 
 namespace Editing1\JetBrains;
@@ -10,30 +17,36 @@ namespace Editing1\JetBrains;
 use Debugging\JetBrains\Person;
 
 $people = array();
-$people[] = new Person('Maarten');
+$people[] = new Person('Trillian');
 
 foreach ($people as $person) {
-    // 1. Autocompletion on $person will not show anything about the Person class.
+    // 1. Auto completion on $person will not show anything about the Person class.
     //    Type $person-> and see for yourself.
-
     // $person->
+
 
     // 2. We can solve this!
-    //    Find the $people = array(); and add a PHPDoc comment: /** @var $people Person[] */ - this tells PhpStorm the array contains Person objects.
-    //
-    //    Type $person-> and see for yourself that we now have autocompletion.
-
+    //    Find the $people = array(); (line 17) and add a PHPDoc comment above:
+    //        /** @var $people Person[] */
+    //    This tells PhpStorm the array contains Person objects.
+    //    Type $person-> and see for yourself that we now have completion.
     // $person->
 
-    // 3. Place the cursor on $person and hit Ctrl+Q (F1). See quick documentation about the variable.
-    // 4. Place the cursor on Person and hit Ctrl+Q (F1). See quick documentation about the type.
-    // 5. Navigate to the Person class and add PHPDoc on the class. Add several @property annotations, for example:
-    //    @property string Address Address of the person
-    //    @property string Country Person country
-    //    While these properties don't actually exist, PhpStorm parses the annotations and will provide Address and Country
-    //    when using the Person class. Try it here, by typing: $person->A
+
+    // 3. Place the cursor on $person and use Quick Documentation.
+
+    // 4. Place the cursor on Person and use Quick Documentation.
+
+    // 5. Navigate to the Person class and add PHPDoc annotations on the class.
+    //    Add several @property annotations, for example:
+    //        @property string Address Address of the person
+    //        @property string Country Person country
+    //    While these properties don't actually exist, PhpStorm parses the
+    //    annotations and will provide Address and Country when using completion
+    //    on the Person class. Try it here, by typing: $person->A
     //
     //    Note PhpStorm also supports @method, @type, @property, @deprecated, @global, @name, @param, @static, @since, @link, @mixin, ...
-
     // $person->A
+
+
 }
