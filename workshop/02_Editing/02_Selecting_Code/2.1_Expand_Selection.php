9a224bd8defdeb714634bcc325ed006e0df405bd
Gary <gary@hock.in>
2017-05-02 10:14:53 +0200

    Tweaked 2.1
    (cherry picked from commit 1a5338d)

--- workshop/02_Editing/02_Selecting_Code/2.1_Expand_Selection.php
+++ workshop/02_Editing/02_Selecting_Code/2.1_Expand_Selection.php
@@ -1,28 +1,37 @@
 <?php
 /**
- * Selecting Code
- *
- * Various ways of selecting code.
+ * Expand and Shrink Selection
  *
  * Ctrl+W / Ctrl+Shift+W (Windows/Linux)
  * Alt+Up / Alt+Down (Mac OS X)
+ *
+ * Expand (or Shrink) the selection to the next logical boundary
  */
 
 namespace Editing2\JetBrains;
 
 use Exception;
 
-class SelectingCode {
-    // 1. Place the caret inside the $importantValue variable and expand selection to select it.
-    //    Expand again to select complete statement without ;.
-    //    Expand again to select complete line.
-    //    Expand again to select function body.
-    //    Expand again to select entire function.
-    //    Expand again to select entire class.
-    //    Expand again to select entire namespace.
-    //    Expand again to select entire file.
-    // 2. Shrink selection to until the extendAndShrinkSelection() function is selected.
-    public function extendAndShrinkSelection() {
+class SelectingCode
+{
+    // 1. Place the caret inside the $importantValue variable and Expand Selection to select it.
+    //    Expand again to select the complete statement without ;
+    //    Expand again to select the complete statement with the ;
+    //    Expand again to select the complete line
+    //    Expand again to select the function body
+    //    Expand again to select the entire function
+    //    Expand again to select the entire class
+    //    Expand again to select the entire namespace
+    //    Expand again to select the entire file
+    // 2. Shrink Selection until the entire extendAndShrinkSelection() function is selected.
+
+    /** @var string */
+    public $Name;
+    /** @var int */
+    public $Age;
+
+    public function extendAndShrinkSelection()
+    {
         $importantValue = 32;
         if ($importantValue > 42) {
             try {
@@ -34,10 +43,19 @@ class SelectingCode {
         }
     }
 
-    /** @var string */ public $Name;
-    /** @var int */ public $Age;
-    public function run() { }
-    public function sayHello() { }
-    public function sayGoodbye() { }
-    public function veryLongFunctionName() { }
+    public function run()
+    {
+    }
+
+    public function sayHello()
+    {
+    }
+
+    public function sayGoodbye()
+    {
+    }
+
+    public function veryLongFunctionName()
+    {
+    }
 }
