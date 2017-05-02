e5b834d5ad9e9f6a15ecd28092f665257b859a0d
Gary <gary@hock.in>
2017-05-02 10:35:07 +0200

    Tweaked 5.1
    (cherry picked from commit 04ada48)

--- workshop/02_Editing/05_Intentions/5.1_Intentions.php
+++ workshop/02_Editing/05_Intentions/5.1_Intentions.php
@@ -2,33 +2,42 @@
 /**
  * Intentions
  *
- * Let the IDE figure out possible actions from context and execute them.
+ * Show Intention Actions (aka Quickfixes)
  *
  * Alt+Enter (Windows/Linux/Mac OS X)
+ *
+ * Let the IDE figure out possible actions from context and execute them
  */
 
 namespace Editing5\JetBrains;
 
-class Intentions {
-    // 1. Flip '==' intention. Place the cursor over "$variable" inside the if statement and hit Alt+Enter.
-    //    We can now flip $variable == true and make it read true == $variable.
-    public function flipIntention() {
+class Intentions
+{
+    // 1. Flip '===' intention. Place the cursor over `$variable` inside the if
+    //    statement invoke Show Intention Actions (also known as Quickfixes).
+    //    We can now flip `$variable === true` and make it read `true === $variable`.
+
+    public function flipIntention()
+    {
         $variable = true;
-        if ($variable == true) {
+        if ($variable === true) {
 
         }
         echo 'This may be throwing an Exception. Riiiight!';
     }
 
-    // 2. Create File intention. Place the caret on "nonexistingfile.php" and hit Alt+Enter.
-    //    We can now generate the file.
-    public function createFileIntention($items) {
+    // 2. Create File intention. Place the caret on `nonexistingfile.php` and
+    //    invoke Quickfixes.
+    //    Generate the file using the Create File intention.
+    public function createFileIntention($items)
+    {
         include 'nonexistingfile.php';
     }
 
     // 3. Simplify Boolean Expression Intention. Place the caret over the last statement in the function.
-    //    Hit Alt+Enter and let the IDE simplify the statement.
-    public function simplifyIntention() {
+    //    Use the intention to IDE simplify the statement.
+    public function simplifyIntention()
+    {
         $a = 0;
         $b = 1;
         echo !($a && !$b);
