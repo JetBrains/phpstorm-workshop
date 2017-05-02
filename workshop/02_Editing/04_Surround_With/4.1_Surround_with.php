1d79699bbb6f07aee732677805b0666cd5bd2cdc
Gary <gary@hock.in>
2017-05-02 09:25:13 +0100

    Tweaked 4.1

--- workshop/02_Editing/04_Surround_With/4.1_Surround_with.php
+++ workshop/02_Editing/04_Surround_With/4.1_Surround_with.php
@@ -2,23 +2,30 @@
 /**
  * Surround With
  *
- * Wrap selected text with new content, e.g. try/catch or if statement.
- *
  * Ctrl+Alt+T (Windows/Linux)
  * Alt+Command+T (Mac OS X)
- * Alt+Enter
+ *
+ * Show Intention Actions
+ *
+ * Alt+Enter (Windows/Linux/Mac OSX)
+ *
+ * Wrap the selected text with relevant content (e.g. try/catch or if statement)
+ * NOTE: You can also invoke contextual surrounds using Show Intention Actions
  */
 
 namespace Editing4\JetBrains;
 
-class SurroundWith {
+class SurroundWith
+{
     // 1. Wrap the statement in the following function in a try/catch block.
-    public function surroundWithTryCatch() {
+    public function surroundWithTryCatch()
+    {
         echo 'This may be throwing an Exception. Riiiight!';
     }
 
-    // 2. Place the caret on the $items[0] element. Hit Alt+Enter to surround with an if(empty(....)) check.
-    public function division($items) {
+    // 2. Place the caret on the $items[0] element. Hit Alt+Enter to surround with an if(!empty(....)) check.
+    public function division($items)
+    {
         return $items[0] / $items[1];
     }
 }
