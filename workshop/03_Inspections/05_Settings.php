4b4816218869d360f9b47d9df1d700b78d547edb
Gary <gary@hock.in>
2017-05-04 14:47:12 +0100

    Tweaked 03_05

--- workshop/03_Inspections/05_Settings.php
+++ workshop/03_Inspections/05_Settings.php
@@ -2,15 +2,17 @@
 /**
  * Inspections - Settings
  *
- * Enable/disable inspections, see examples and documentation.
- *
  * Ctrl+Alt+S (Windows/Linux)
  * Command+, (Mac OS X)
+ *
+ * Enable/disable inspections, see examples and documentation.
  */
 
-// 1. Open the project settings | inspections.
+// 1. Open the Settings pane then Editor | Inspections.
 // 2. Find the Expression Result Unused inspection and see why this is probably a code bug.
-// 3. Find the Usage of Silence Operator inspection. Enable it and make it an Error. The code below should now show a warning.
+// HINT: You can search for inspections by name by typing next to the magnifying glass above the inspection list.
+// 3. Find the Usage of Silence Operator inspection. Enable it and make it an Error. The code below should now show an
+//    error.
 // 4. Find the Missing Break Statement inspection and make it an Error.
 // 5. Disable the Inconsistent Return Points inspection.
 
