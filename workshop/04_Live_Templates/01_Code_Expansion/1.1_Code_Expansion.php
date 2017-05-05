448b3745c97a4f4e1cffde8258ecf4e3df2509fe
Gary <gary@hock.in>
2017-05-05 09:25:10 +0100

    Tweaked 04_01

--- workshop/04_Live_Templates/01_Code_Expansion/1.1_Code_Expansion.php
+++ workshop/04_Live_Templates/01_Code_Expansion/1.1_Code_Expansion.php
@@ -2,20 +2,24 @@
 /**
  * Code Expansion
  *
+ * Tab (Windows/Linux/Mac OS X)
+ *
  * Expand text shortcut using template.
  * Expands into code with variable “hotspots”.
- * Hotspot can be linked to a macro, such as “suggest type of variable”, “suggest name for variable”, “clipboard content”, “new GUID”, etc.
- * Tab to move between hotspots.
+ * Hotspot can be linked to a macro, such as “suggest type of variable”, “suggest name for variable”, “clipboard content”,
+ *     “new GUID”, etc.
+ * Tab to move to next hotspot.
  * Template can define end point for caret.
- *
- * Tab (Windows/Linux/Mac OS X)
  */
 
 namespace LiveTemplates1\JetBrains;
 
-// 1. Open the IDE settings | Live Templates and inspect the various live templates available.
+// 1. Open the Setting Pane then Editor | Live Templates and inspect the various live templates available.
 //    We can see the available live templates here or create our own.
-class CodeExpansion {
+// HINT: Notice how we have different live templates depending on the type of file we are editing
+
+class CodeExpansion
+{
     // 2. Create a protected function firstFunction().
     //    Type prof and hit TAB. Enter firstFunction in the first hotspot and hit TAB again.
     //    We could accept arguments or choose to have none. Hit TAB again to move the caret to the function body.
@@ -26,13 +30,15 @@ class CodeExpansion {
 
     // pubsf <TAB>
 
-    protected function secondFunction(){
-        $numbers = array(1,2,3,4,5,6,7,8,9);
+    protected function secondFunction()
+    {
+        $numbers = array(1, 2, 3, 4, 5, 6, 7, 8, 9);
 
         // 4. Loop over the $numbers array. Type "fore" and hit TAB.
         //    In the first hotspot, enter $numbers and hit TAB again.
-        //    In the second hotspot, enter a variable name and hit TAB to move to the loop body.
+        //    In the second hotspot, accept the default variable name and hit TAB to move to the loop body.
 
         // fore <TAB>
+
     }
 }
