8a63ffd4769c23c4eef4fbb13d799381e3f08533
Gary <gary@hock.in>
2017-05-05 12:02:26 +0100

    Updated 05_13

--- workshop/05_Refactoring/13_Rename.php
+++ workshop/05_Refactoring/13_Rename.php
@@ -2,28 +2,33 @@
 /**
  * Rename
  *
- * Rename symbols, automatically correcting all references in the code.
- *
  * Shift+F6 (Windows/Linux/Mac OS X)
+ *
+ * Rename symbols, automatically correcting all references in the code.
  */
 
 namespace Refactoring13\JetBrains;
 
-// 1. Rename the Person class to Customer. Note that all usages will be updated.
-//    Also search in comments so that these are updated too.
-class Person {
+// 1. Rename the Person class to Customer by using Rename and then over-typing. Notice that all usages will be updated.
+// 2. Undo the last rename, then rename the Person class using Rename refactor.
+//    Use Shift+F6 again when over-typing to enable the advanced dialog, enable the Search in comments and strings.
+//    option and perform the rename - notice how the preview box shows the comments can be updated.
+class Person
+{
     // ...
 
-    // 2. Rename the sayHello() function to sayGoodbye().
-    public function sayHello() {
+    // 3. Rename the sayHello() function to sayGoodbye().
+    public function sayHello()
+    {
         return 'Hello!';
     }
 
-    // 3. Rename the $name parameter to $string. Note that the PHPDoc block is updated as well.
+    // 4. Rename the $name parameter to $string. Note that the PHPDoc block is updated as well.
     /**
      * @param string $name Name of the person.
      */
-    public function greet($name) {
+    public function greet($name)
+    {
 
     }
 }
@@ -31,4 +36,4 @@ class Person {
 $person = new Person(); // creates a new Person
 $person->sayHello();
 
-// 4. Rename the current file to "13_Rename_done.php". Note that all usages of the file will be updated.
+// 5. Rename the current file to "13_Rename_done.php". Note that all usages of the file will be updated.
