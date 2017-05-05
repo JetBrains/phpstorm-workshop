c4099ae21c57a7d61e41613f03a63e6a83c5c3f4
Gary <gary@hock.in>
2017-05-05 13:05:58 +0200

    Updated 05_14
    (cherry picked from commit eeaa036)

--- workshop/05_Refactoring/14_Safe_Delete.php
+++ workshop/05_Refactoring/14_Safe_Delete.php
@@ -2,22 +2,24 @@
 /**
  * Safe Delete
  *
- * Safely remove code or symbols.
- *
  * Alt+Delete (Windows/Linux)
  * Command+Delete (Mac OS X)
+ *
+ * Safely remove code or symbols.
  */
 
 namespace Refactoring14\JetBrains;
 
 require_once 'Code/MoreCode.php';
 
-// 1. Safe Delete the CoolPerson class from the Project Tool Window.
+// 1. Safe Delete the `14_Safe_Delete.php` file from the Project Pane.
 //    Note that it is not safe to delete the file as it is being used elsewhere.
-class CoolPerson {
+class CoolPerson
+{
     protected $_name;
 
-    public function __construct($name) {
+    public function __construct($name)
+    {
         $this->_name = $name;
     }
 }
