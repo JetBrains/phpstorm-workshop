7a2cf58fe876f7c5565f95689dc18258778b3089
Gary <gary@hock.in>
2017-05-05 11:42:40 +0200

    Updated 05_03 and 05_04
    I don't think `Clone` exists as a shortcut in the platform anymore - can you confirm @MaXal?
    
    (cherry picked from commit a601578)

--- workshop/05_Refactoring/03_Copy_Clone.php
+++ workshop/05_Refactoring/03_Copy_Clone.php
@@ -2,16 +2,17 @@
 /**
  * Copy/Clone
  *
- * Copy a class, file or directory to another directory or clone it within the same directory.
- *
  * F5 (copy), Shift+F5 (clone) (Windows/Linux/Mac OS X)
+ *
+ *
+ * Copy a class, file or directory to another directory or clone it within the same directory.
  */
 
 namespace Refactoring3\JetBrains;
 
 // 1. Copy the 01_Refactor_This.php file to the Code directory.
 //    Do this from the Project Tool Window and do not touch the mouse.
-//    (Alt+1 / Command+1, arrow keys, F5)
+//    HINT: Alt+1/Command+1, arrow keys, F5
 
 // 2. Clone the current file and name it "03_Copy_Clone_(cloned).php".
 //    Use the Shift+F5 keyboard shortcut from within the editor.
