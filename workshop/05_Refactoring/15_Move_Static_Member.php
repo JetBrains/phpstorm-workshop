4de3e590ca5d1f236936a0758a9aa87ac5f0c654
Gary <gary@hock.in>
2017-05-05 12:07:57 +0100

    Updated 05_15 05_16

--- workshop/05_Refactoring/15_Move_Static_Member.php
+++ workshop/05_Refactoring/15_Move_Static_Member.php
@@ -2,24 +2,26 @@
 /**
  * Move Static Member
  *
- * Move static fields and methods to another type.
- *
  * F6 (Windows/Linux/Mac OS X)
+ *
+ * Move static fields and methods to another type.
  */
 
 namespace Refactoring15\JetBrains;
 
 // 1. Move the static method "split" from the class Utils to a more specialized StringUtils.
-class Utils {
+class Utils
+{
     public static function split($subject, $delimiter)
     {
         return explode($delimiter, $subject);
     }
 }
 
-class StringUtils {
+class StringUtils
+{
 }
 
-// 2. Note that calls to Utils::split() have updated to StringUtils::split().
+// 2. Notice that calls to Utils::split() have updated to StringUtils::split().
 $myArray = Utils::split('This is a test', ' ');
 $myCsvRecord = Utils::split('Maarten;JetBrains', ';');
