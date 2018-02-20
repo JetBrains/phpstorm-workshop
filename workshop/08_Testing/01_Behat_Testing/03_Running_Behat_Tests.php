5f101be23aca8b3e836de14f1ea5bde18e0bd39f
jetbrains <dmitry.tronin@jetbrains.com>
2018-02-20 18:54:45 +0300

    Addressing https://github.com/JetBrains/phpstorm-workshop/issues/42

---
+++ workshop/08_Testing/01_Behat_Testing/03_Running_Behat_Tests.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Running Behat Tests.
+ *
+ * Last Modified: 2/20/2018
+ */
+namespace BehatTesting\JetBrains;
+
+//    Now we're ready to run our Behat tests:
+//    - right click a "basket.feature" file > select "Create basket.feature" with a "B" (for Behat) logo against it.
+//
+//        IDE would fill out all the necessary settings for us.
+//        In case you want to fine tune those settings - feel free to either edit automatically created configuration
+//        or create a Behat run configuration anew using Run > Edit Configurations > "+" sign > Behat. See more
+//        about available options at https://www.jetbrains.com/help/phpstorm/run-debug-configuration-behat.html
+//
+//    Let's stick to default for now. Click "Ok" and a "Run" tool window would appear with test results!
+//
+//    You should receive a result similar to the one below:
+//
+//            1 scenario (1 passed)
+//            4 steps (4 passed)
+//            0m0.04s (7.97Mb)
+//
+//            Process finished with exit code 0
+//
+//    You can verify this running `vendor\bin\behat` in terminal.
