6fcac60623d3fb9ba53c5333b58f1b96e86634e2
Anton Monakov <anton.monakov@jetbrains.com>
2018-11-09 17:44:39 +0300

    'Clone' command is no longer mentioned

--- workshop/05_Refactoring/03_Copy_Clone.php
+++ workshop/05_Refactoring/03_Copy_Clone.php
@@ -13,8 +13,4 @@ namespace Refactoring3\JetBrains;
 // 1. Copy the 01_Refactor_This.php file to the Code directory.
 //    Do this from the Project Tool Window and do not touch the mouse.
 //    HINT: Alt+1/Command+1, arrow keys, F5
-
-// 2. Clone the current file and name it "03_Copy_Clone_(cloned).php".
-//    Use the F5 keyboard shortcut from within the editor.
-
-// 3. Copy the current file to the Code directory. Drag/drop it using Ctrl key pressed.
+// 2. Copy the current file to the Code directory. Drag/drop it using Ctrl key pressed.
