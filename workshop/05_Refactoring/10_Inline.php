9607f14d9dd8639b3d52bf8745e4b8b32d8a5090
Anton Monakov <anton.monakov@jetbrains.com>
2018-11-09 19:21:24 +0300

    Extended to include the Inline Method Refactoring

--- workshop/05_Refactoring/10_Inline.php
+++ workshop/05_Refactoring/10_Inline.php
@@ -5,7 +5,7 @@
  * Ctrl+Alt+N (Windows/Linux)
  * Alt+Command+N (Mac OS X)
  *
- * Replace redundant variables or functions with the full expression. It is the opposite of Extract Method.
+ * Replace redundant variables, functions, or methods with the full expression. It is the opposite of the Extract refactoring.
  */
 
 namespace Refactoring\JetBrains;
@@ -13,3 +13,13 @@ namespace Refactoring\JetBrains;
 // 1. Place the caret over $name and inline the variable.
 $name = 'Benji';
 echo $name;
+
+
+
+function printName($name)
+{
+    echo $name;
+}
+
+// 2. Place the caret over printName() and inline the function.
+printName("John");
