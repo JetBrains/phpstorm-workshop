d8953f134e94d26cebe3f93394d6d18b90ab2e91
Anton Monakov <anton.monakov@jetbrains.com>
2018-11-09 20:09:12 +0300

    Added the info on Multiline TODOs

--- workshop/07_Todo/01_Todo.php
+++ workshop/07_Todo/01_Todo.php
@@ -13,4 +13,11 @@ namespace Todo1\JetBrains;
 // 3. Double-click or use the F4 shortcut to go to the first todo comment.
 // 4. Find the TODO comment in index.html. TODO works in every language supported by the IDE.
 // 5. Add a new todo item  by creating a comment followed by fixme or todo, for example:
+
 // todo: create a new comment below this line
+
+// 6. Add a multiline todo by indenting the comment lines that follow the initial line:
+
+// TODO: this is a longer todo comment.
+//  It spans over several lines
+//  but is treated as a single item.
