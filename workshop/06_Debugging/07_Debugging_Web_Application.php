502360bf251fb5a2a301cffb10456afd84fb3994
Anton Monakov <anton.monakov@jetbrains.com>
2019-02-25 18:01:48 +0300

    Added a note on the occupied port 9000

--- workshop/06_Debugging/07_Debugging_Web_Application.php
+++ workshop/06_Debugging/07_Debugging_Web_Application.php
@@ -7,12 +7,12 @@
 
 namespace Debugging7\JetBrains;
 
-// 0. Make sure that you've entered the valid IP address in  docker-compose.yml
+// 0. Make sure that you've entered the valid IP address in docker-compose.yml
 // 1. Make sure that you've executed "docker-compose up"
 // 2. Visit http://www.jetbrains.com/phpstorm/marklets/ and generate bookmarklets for your debugger (Zend Debugger or Xdebug).
 //    Drag at least the Start Debugger bookmarklet to the bookmarks bar.
 //
-//    If you prefer using a simple browser extension, refer http://confluence.jetbrains.com/display/PhpStorm/Browser+Debugging+Extensions
+//    If you prefer using a simple browser extension, refer to https://www.jetbrains.com/help/phpstorm/browser-debugging-extensions.html
 //    and install one. For example for Chrome, install https://chrome.google.com/webstore/detail/xdebug-helper/eadndfjplgieldjbigjakmdgkmoaaaoc.
 // 3. Open http://localhost:8081/07_Debugging_Web_Application.php or click browser icon in the right upper corner of editor
 // 4. In PhpStorm, use the Run | Start Listening for PHP Debug Connections menu.
